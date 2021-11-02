bf5470def6206bfbe764d430e5574b6e962945d9
Track-tor <48454411+Track-tor@users.noreply.github.com>
2021-11-02 17:52:48 -0300

    improved fuse effects by adding a particle system and a light source to it

--- Assets/Scripts/ActivableBehaviour.cs
+++ Assets/Scripts/ActivableBehaviour.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class ActivableBehaviour : MonoBehaviour
 {
     [SerializeField]private ActivableObject activableObject;
-    private BoxCollider2D fuseCollider;
     private SpriteRenderer spriteRenderer;
+    private ParticleSystem particles;
+    private BoxCollider2D fuseCollider;
+    public Light2D fireLight;
 
     private void Start()
     {
         fuseCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        particles = GetComponent<ParticleSystem>();
+        fireLight = GetComponent<Light2D>();
+        fireLight.enabled = false;
+        particles.Stop();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,7 +26,10 @@ public class ActivableBehaviour : MonoBehaviour
         if (collision.CompareTag("Mr Bright"))
         {
             activableObject.Activate();
-            spriteRenderer.color = Color.red;
+            spriteRenderer.color = new Color(0.3f, 0.3f, 0.3f);
+            particles.Play();
+            fuseCollider.enabled = false;
+            fireLight.enabled = true;
         }
     }
 }
